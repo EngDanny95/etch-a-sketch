7ae0d25348a40b98de525ca19b6a247fe77092d0
Danny <EngDanny95@gmail.com>
2024-04-07 00:34:51 +0300

    build(css): stretch divs w/o content & add margins

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ for (let i = 1; i < 17; i++){
     for (let j=1; j < 17; j++){
         const divs = document.createElement("div")
         container.appendChild(divs)
-        divs.textContent=`${j}`
+        
     }
 }
