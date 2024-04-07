7eb42bfe675fa36198f5a9279968e9a06895cdec
Danny <EngDanny95@gmail.com>
2024-04-07 20:28:36 +0300

    feat(hover): color square grids mouse passing over

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const container = document.querySelector("div")
 for (let i = 1; i < 17; i++){
     for (let j=1; j < 17; j++){
         const divs = document.createElement("div")
+        divs.addEventListener("mouseover",()=>{
+            divs.style.backgroundColor = "red";
+        })
         container.appendChild(divs)
-        
     }
 }
