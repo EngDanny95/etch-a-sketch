8b4aa1463528c5748942d9c36ab7fd25f417dbbd
Danny <EngDanny95@gmail.com>
2024-04-10 00:00:14 +0300

    build(flex): generate custom grid size
    Relied heavily on creating variable in css and
    manipulating its value in js for custom grid size
    prompt in coaporation with flexbox-basis property.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const container = document.querySelector("div")
-for (let i = 1; i < 17; i++){
-    for (let j=1; j < 17; j++){
+let grid = prompt("Input the number of squares per side for the grid between 1 and 100");
+let factor = (1 / grid);
+if (grid<=100 && grid>=1){
+for (let i = 1; i <= grid; i++) {
+    for (let j = 1; j <= grid; j++) {
         const divs = document.createElement("div")
-        divs.addEventListener("mouseover",()=>{
+        divs.style.setProperty('--x', factor) //modify the css variable since
+        // js variable arthmatic is not possible in css property snippet
+        divs.addEventListener("mouseover", () => {
             divs.style.backgroundColor = "red";
         })
         container.appendChild(divs)
     }
+}
+}
+else{
+    alert("wrong input")
 }
