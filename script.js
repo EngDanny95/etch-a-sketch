5244aa95e03f44142b707189624cfe225b002c69
Danny <EngDanny95@gmail.com>
2024-04-17 13:49:20 +0300

    build(js): loop the prompt to enter correct value
    The prompt now will alert the user with wrong value
    message until the user enters the correct value.
    User can cancel the operation by clicking cancel
    button

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const container = document.querySelector("div")
-let grid = prompt("Input the number of squares per side for the grid between 1 and 100");
+let grid = prompt("Enter the number of squares per side for the grid between 1 and 100");
+while (grid > 100  || grid==0 || grid<0 || isNaN(grid) || grid%1) {
+    // == includes an empty string as well as zero
+    // < & == are used instead of <= 0 operator because <= converts non zero
+    // value to zero including null which is what the cancel button value is
+    // Nan checks if non numberic value is entered in the prompt
+    // % makes sure non decimals values are entered
+    alert("wrong input")
+    grid = prompt("Enter the number of squares per side for the grid between 1 and 100");
+}
 let factor = (1 / grid);
-if (grid<=100 && grid>=1){
 for (let i = 1; i <= grid; i++) {
     for (let j = 1; j <= grid; j++) {
         const divs = document.createElement("div")
@@ -12,8 +20,4 @@ for (let i = 1; i <= grid; i++) {
         })
         container.appendChild(divs)
     }
-}
-}
-else{
-    alert("wrong input")
 }
