7dfdb8c461429d122acb9f5f6c8c04b81f96cd6a
Danny <EngDanny95@gmail.com>
2024-04-29 13:48:35 +0300

    feat(color): darkern random grid color on hover
    By using color mixer function, original rgb color
    opacity is decreased until only the black color is
    left to show full dark black color.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const body = document.querySelector("body")
 let container = document.querySelector("div")
 let clickable = document.createElement("button")
-body.insertBefore(clickable,container)
-clickable.textContent="Grid size"
-clickable.addEventListener("click",()=>{
+body.insertBefore(clickable, container)
+clickable.textContent = "Grid size"
+
+clickable.addEventListener("click", () => {
     let grid = prompt("Enter the number of squares per each side of the grid between 1 and 100");
-    while (grid > 100  || grid==0 || grid<0 || isNaN(grid) || grid%1) {
+    while (grid > 100 || grid == 0 || grid < 0 || isNaN(grid) || grid % 1) {
         // == includes an empty string as well as zero
         // < & == are used instead of <= 0 operator because <= converts non zero
         // value to zero including null which is what the cancel button value is
@@ -15,16 +16,33 @@ clickable.addEventListener("click",()=>{
         grid = prompt("Enter the number of squares per each side of the grid between 1 and 100");
     }
     let factor = (1 / grid);
-    while (container.hasChildNodes()){
+    while (container.hasChildNodes()) {
         container.removeChild(container.firstChild)
     }
-     for (let i = 1; i <= grid; i++) {
+    for (let i = 1; i <= grid; i++) {
         for (let j = 1; j <= grid; j++) {
             let divs = document.createElement("div")
             divs.style.setProperty('--x', factor) //modify the css variable since
             // js variable arthmatic is not possible in css property snippet
+            let num = 100
             divs.addEventListener("mouseover", () => {
-                divs.style.backgroundColor = "red";
+                let rjs = Math.floor(Math.random() * 256) // red
+                let gjs = Math.floor(Math.random() * 256) // green
+                let bjs = Math.floor(Math.random() * 256) // blue
+                if (num == 0)
+                    opacityjs = num + "%"
+
+                else{
+                    num = num - 10
+                    opacityjs = num + "%"
+                }
+                divs.style.setProperty('--rcss', rjs)
+                divs.style.setProperty('--gcss', gjs)
+                divs.style.setProperty('--bcss', bjs)
+                divs.style.setProperty('--opacitycss', opacityjs)
+
+                divs.style.backgroundColor = "color-mix(in sRGB, rgb(var(--rcss),var(--gcss),var(--bcss)) var(--opacitycss), black)"
+
             })
             container.appendChild(divs)
         }
